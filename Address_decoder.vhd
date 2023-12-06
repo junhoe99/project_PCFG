e35d5a841be2622140b6bbb16aa21316344fa3e3
junhoe99 <147131312+junhoe99@users.noreply.github.com>
2023-12-06 15:57:48 +0900

    Update Address_decoder.vhd

--- Address_decoder.vhd
+++ Address_decoder.vhd
@@ -1,21 +1,7 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 2023/12/04 21:31:34
--- Design Name: 
--- Module Name: Address_decoder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+
+-- Module Name: address_decoder - Behavioral
+
 ----------------------------------------------------------------------------------
 
 
@@ -31,37 +17,45 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-
 entity address_decoder is
-  port (
-    m_pc_address: in std_logic_vector(8 downto 0); -- input from PC
-    A: out std_logic_vector(7 downto 0); -- output to A
-    m_CW_addr: out std_logic; -- output to Control Word register
-    m_DA_start_addr: out std_logic; -- output to DA start
-    m_DA_stop_addr: out std_logic; -- output to DA stop
-    m_MEM_addr: out std_logic; -- output to memory
-    m_XCLR_addr: out std_logic; -- output to PC mode reset or DA mode reset
-    m_db_en_b: out std_logic; -- output to data bus enable
-    m_PCS_b: out std_logic -- output to peripheral chip select
+  Port ( 
+  m_pc_address	: in std_logic_vector(8 downto 0); -- 9bit address bus
+  m_pc_address_valid :in std_logic;
+  
+  CW_addr :out std_logic;
+  MEM_addr :out std_logic;
+  XCLR_addr :out std_logic;
+  DA_start_addr :out std_logic;
+  DA_stop_addr	:out std_logic;
+  
+  db_en_b :out std_logic; -- pc mode (Directional Buffer enable control)
+  PCS_b	:out std_logic
   );
 end address_decoder;
 
-architecture behavioral of address_decoder is
+architecture Behavioral of address_decoder is
 begin
-  process (m_pc_address)
-  begin
+
+
+    process(m_pc_address_valid)
+begin
     case m_pc_address is      
-      when "10110000" => m_ <= '1'; -- address 180H :
-      when "10110111" => m_ <= '1'; -- address 183H
-      when "10111000" => m_CW_addr <= '1'; -- address 184H
-      when "10111001" => m_MEM_addr <= '1'; -- address 185H
-      when "10111010" => m_XCLR_addr <= '1'; -- address 186H
-      when "10111000" => m_DA_start_addr <= '1'; -- address 188H
-      when "10111001" => m_DA_stop_addr <= '1'; --address 189H
-      when others => A <= "00000000"; -- invalid address
+      when "1100000XX" => PCS_b <= '0'; -- address 180H-183H :
+      when "110000XXX" => db_en_b <= '0'; -- address 180H-187H
+      when "110000100" => CW_addr <= '1'; -- address 184H
+      when "110000101" => MEM_addr <= '1'; -- address 185H
+      when "110000110" => XCLR_addr <= '1'; -- address 186H
+      when "110001000" => DA_start_addr <= '1'; -- address 188H
+      when "110001001" => DA_stop_addr <= '1'; --address 189H
+      when others  =>
+      PCS_b <= '1';
+      db_en_b <= '1';
+      CW_addr <= '0';
+      MEM_addr <= '0';
+      XCLR_addr <= '0';
+      DA_start_addr<= '0';
+      DA_stop_addr<= '0';
     end case;
-    --db_en, PCS is not yet completed
-    m_db_en_b <= '0' when m_pc_address >= "10110000" and m_pc_address <= "10111011" else '1'; -- data bus enable for addresses 180H to 18BH
-    m_PCS_b <= '0' when m_pc_address >= "10110000" and m_pc_address <= "10111011" else '1'; -- peripheral chip select for addresses 180H to 18BH
-  end process;
-end behavioral;
+
+end process;
+end Behavioral;
