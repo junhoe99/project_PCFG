b98d4f5b54fc64fbe78e7e07cfdb4e7e604a04b1
junhoe99 <147131312+junhoe99@users.noreply.github.com>
2023-12-06 16:20:55 +0900

    Update Address_decoder.vhd

--- Address_decoder.vhd
+++ Address_decoder.vhd
@@ -37,25 +37,38 @@ architecture Behavioral of address_decoder is
 begin
 
 
-    process(m_pc_address_valid)
+    process(m_pc_address_valid, m_pc_address)
 begin
-    case m_pc_address is      
-      when "1100000XX" => PCS_b <= '0'; -- address 180H-183H :
-      when "110000XXX" => db_en_b <= '0'; -- address 180H-187H
-      when "110000100" => CW_addr <= '1'; -- address 184H
-      when "110000101" => MEM_addr <= '1'; -- address 185H
-      when "110000110" => XCLR_addr <= '1'; -- address 186H
-      when "110001000" => DA_start_addr <= '1'; -- address 188H
-      when "110001001" => DA_stop_addr <= '1'; --address 189H
-      when others  =>
-      PCS_b <= '1';
-      db_en_b <= '1';
-      CW_addr <= '0';
-      MEM_addr <= '0';
-      XCLR_addr <= '0';
-      DA_start_addr<= '0';
-      DA_stop_addr<= '0';
-    end case;
+    -- m_pc_addr_valid가 1일때만 address decoder를 실행
+    if m_pc_address_valid = '1' then
+        case m_pc_address is      
+          when "1100000XX" => PCS_b <= '0'; -- address 180H-183H :   
+          when "110000XXX" => db_en_b <= '0'; -- address 180H-187H
+          when "110000100" => CW_addr <= '1'; -- address 184H
+          when "110000101" => MEM_addr <= '1'; -- address 185H
+          when "110000110" => XCLR_addr <= '1'; -- address 186H
+          when "110001000" => DA_start_addr <= '1'; -- address 188H
+          when "110001001" => DA_stop_addr <= '1'; --address 189H
+          when others => 
+            -- m_pc_address가 180H~189H가 아닌 경우에 원하는 동작을 정의합니다.
+            PCS_b <= '1';
+            db_en_b <= '1';
+            CW_addr <= '0';
+            MEM_addr <= '0';
+            XCLR_addr <= '0';
+            DA_start_addr <= '0';
+            DA_stop_addr <= '0';
+        end case;
+    else
+        -- m_pc_addr_valid가 0일때는 address decoder를 실행하지 않고, 모든 신호를 0으로 만듭니다.
+        PCS_b <= '1';
+        db_en_b <= '1';
+        CW_addr <= '0';
+        MEM_addr <= '0';
+        XCLR_addr <= '0';
+        DA_start_addr <= '0';
+        DA_stop_addr <= '0';
+    end if;
 
 end process;
 end Behavioral;
